9b57172fccfbeb0f083d070926070ead222ecb2a
andrehiro <130809449+andrehiro@users.noreply.github.com>
2023-10-28 17:05:25 -0700

    Add files via upload

---
+++ practica3.5.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/conjunto/<conjunto_param>/<elemento>/')
+def agregarElementoConjunto(conjunto_param, elemento):
+    conjunto = conjunto_param.split(',')
+    if elemento not in conjunto:
+        conjunto.append(elemento)
+        return jsonify(True,conjunto)
+    else:
+        return jsonify(False,conjunto)
+
+#http://localhost:5000/dict/{"uno",1,2.2,(1,2),"dos"}
+#http://localhost:5000/dict/{"uno",1,2.2,(1,2),"dos"}
